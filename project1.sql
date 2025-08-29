6000926daec0543e44257b3b90297413d457ab00
Hoang Long Nguyen <80893626+rekls2311@users.noreply.github.com>
2025-08-29 15:04:47 +1000

    Create project1.sql

---
+++ project1.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE PROJECT1;
+USE PROJECT1;
+create table SALES_DATASET_RFM_PRJ(
+  ordernumber VARCHAR(200),
+  quantityordered VARCHAR(200),
+  priceeach        VARCHAR(200),
+  orderlinenumber  VARCHAR(200),
+  sales            VARCHAR(200),
+  orderdate        VARCHAR(200),
+  status           VARCHAR(200),
+  productline      VARCHAR(200),
+  msrp             VARCHAR(200),
+  productcode      VARCHAR(200),
+  customername     VARCHAR(200),
+  phone            VARCHAR(200),
+  addressline1     VARCHAR(200),
+  addressline2     VARCHAR(200),
+  city             VARCHAR(200),
+  state            VARCHAR(200),
+  postalcode       VARCHAR(200),
+  country          VARCHAR(200),
+  territory        VARCHAR(200),
+  contactfullname  VARCHAR(200),
+  dealsize         VARCHAR(200)
+) 
